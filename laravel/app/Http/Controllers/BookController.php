cbfb48170aa10929e7b44692fe8286ad844d794c
Hirofumi Tanigami <tanigami@gmail.com>
2017-04-12 14:56:02 +0900

    Sends out emails to team memvers when a book is created

--- laravel/app/Http/Controllers/BookController.php
+++ laravel/app/Http/Controllers/BookController.php
@@ -4,6 +4,8 @@ namespace Wagg\Yondel\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Wagg\Yondel\Mail\BookCreated;
 use Wagg\Yondel\Model\Book;
 use Wagg\Yondel\Model\User;
 
@@ -37,19 +39,23 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
+        $team = Auth::user()->currentTeam;
+
         $this->validate($request, [
             'title' => ['required', 'string'],
             'asin' => ['required', 'string', 'unique:books', 'max:512'],
             'recommendation_comment' => ['required', 'string']
         ]);
 
-        Book::create([
-            'team_id' => Auth::user()->current_team_id,
+        $book = Book::create([
+            'team_id' => $team->id,
             'recommender_id' => Auth::user()->id,
             'title' => $request->input('title'),
             'asin' => $request->input('asin'),
             'recommendation_comment' => $request->input('recommendation_comment'),
         ]);
+
+        Mail::to($team->users)->send(new BookCreated($book));
 
         return redirect()->route('book.index');
     }
