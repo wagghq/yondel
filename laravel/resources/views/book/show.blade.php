12b9341aae9cf0a084a5d7fc9e8e360689348263
Hirofumi Tanigami <tanigami@gmail.com>
2017-04-15 02:48:06 +0900

    Who's read table with colors

--- laravel/resources/views/book/show.blade.php
+++ laravel/resources/views/book/show.blade.php
@@ -4,7 +4,7 @@
   <div class="row">
     <div class="columns full">
       <header>
-        <h1>{{ $book->title }}</h1>
+        <h2>{{ $book->title }}</h2>
         <p>Recommended by {{ $book->recommender->name }}</p>
       </header>
     </div>
@@ -14,18 +14,16 @@
 
       <section id="comments">
         <header>
-          <h2>Comments</h2>
+          <h3>Comments</h3>
         </header>
-        @if (count($comments))
-          @foreach ($comments as $comment)
-            <section>
-              <h4>{{ $comment->user->name }} says:</h4>
-              <p>{{ $comment->body }}</p>
-            </section>
-          @endforeach
-        @else
-          <p>No comments.</p>
-        @endif
+
+        @foreach ($comments as $comment)
+          <section>
+            <h4>{{ $comment->user->name }} says:</h4>
+            <p>{{ $comment->body }}</p>
+          </section>
+        @endforeach
+
         <form action="{{ route('bookComment.store', $book->id) }}" method="POST">
           {{ csrf_field() }}
 
@@ -37,26 +35,42 @@
 
     </div>
 
-    <div class="columns medium-6">
+    <hr />f
 
-      <section id="readers">
-        <h2>Readers</h2>
-        @if (count($readers))
+    <div class="row">
+      <div class="columns full">
+
+        <section id="readers">
+          <h2>Who’s read this?</h2>
+
+          <table>
+            <thead>
+              @foreach ($users as $user)
+                <th>{{ $user->name }}</th>
+              @endforeach
+            </thead>
+            <tbody>
+            @foreach ($users as $user)
+              <td class="{{ $readers->contains($user) ? 'read' : 'unread' }}">
+                {{ $readers->contains($user) ? 'Read' : 'Unread' }}
+              </td>
+            @endforeach
+            </tbody>
+          </table>
           <ul>
             @foreach ($readers as $reader)
               <li>{{ $reader->name }}</li>
             @endforeach
           </ul>
-        @else
-          <p>Nobody has read this book.</p>
-        @endif
 
-        <form action="{{ route('book.read', $book->id) }}" method="POST">
-          {{ csrf_field() }}
-          <button type="submit" class="button">I've read this!</button>
-        </form>
-      </section>
 
+          <form action="{{ route('book.read', $book->id) }}" method="POST">
+            {{ csrf_field() }}
+            <button type="submit" class="button">I've read this!</button>
+          </form>
+        </section>
+
+      </div>
     </div>
   </div>
 @endsection
